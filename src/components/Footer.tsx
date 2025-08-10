ce63adc80f06c80230129066fa4cc0f7f9ac089b
Kevin <contact@kevinpaulidor.de>
2025-08-10 13:32:02 +0200

    feat: implement social media icons in Footer component

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,49 @@
-import React from 'react'
+import twitterIcon from "../assets/images/TwitterIcon.svg";
+import facebookIcon from "../assets/images/FacebookIcon.svg";
+import instaIcon from "../assets/images/InstagramIcon.svg";
+import githubIcon from "../assets/images/GitHubIcon.svg";
 
 export default function Footer() {
   return (
-    <footer>Footer</footer>
-  )
+    <footer>
+      <div className="social-icons">
+        <a 
+          href="https://x.com/"
+          target="_blank"
+          rel="noopener noreferrer"
+          >
+          <img src={twitterIcon} alt="twitter Icon" />
+        </a>
+        <a 
+          href="https://www.facebook.com/"
+          target="_blank"
+          rel="noopener noreferrer"
+          >
+          <img src={facebookIcon} alt="facebookIcon" />
+        </a>
+        <a 
+          href="https://www.instagram.com/"
+          target="_blank"
+          rel="noopener noreferrer"
+          >
+          <img src={instaIcon} alt="instaIcon" />
+        </a>
+        <a 
+          href="https://github.com/"
+          target="_blank"
+          rel="noopener noreferrer"
+          >
+          <img src={githubIcon} alt="github Icon" />
+        </a>
+      </div>
+    </footer>
+  );
 }
+
+<a 
+  href="https://www.example.com" 
+  target="_blank" 
+  rel="noopener noreferrer"
+  >
+    Lien vers Example
+</a>
